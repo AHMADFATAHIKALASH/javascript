5d293ecb267a6066a42775cd2d2880f28cceb0a8
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-01-04 19:27:56 -0300

    .

---
+++ aula012/ex010.js
@@ -0,0 +1,9 @@
+var idade = 45
+console.log(` Você tem ${idade} Anos`)
+if(idade < 16){
+    console.log('Não pode votar')
+}else if(idade < 18 || idade > 65 ){
+    console.log('voto opcional')
+}else{
+    console.log('voto obrigatório')
+}
