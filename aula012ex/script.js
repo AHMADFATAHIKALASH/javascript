25bd55fdc7f8aafde8b4ecf2e6073e6f46710fad
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-01-09 21:49:25 -0300

    .

--- aula012ex/script.js
+++ aula012ex/script.js
@@ -7,14 +7,14 @@ var hora = data.getHours()
 msg.innerHTML= `Agora são ${hora} horas.`
 
 if (hora >= 0 && hora <12){
-    img.src ='fotomanha.png'
+    img.setAttribute('src', 'fotomanha.png')
     document.body.style.background = '#e2cd9f'
 } else if(hora>=12 && hora <=18){
-    img ='fototarde.png'
+    img.setAttribute('src', 'fototarde.png')
     document.body.style.background = '#b9846f'
 }else{
     
-    img.src= 'fotonoite.png'
+    img.setAttribute('src', 'fotonoite.png')
     document.body.style.background ='#515154'
 }
 }
