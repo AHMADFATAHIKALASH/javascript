33125f3825c115d7052a532e422436d3fbfdb287
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-01-15 12:39:10 -0300

    .

--- ex014/script.js
+++ ex014/script.js
@@ -2,21 +2,56 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
+    var res = document.getElementById('res')
+    
 
-    if (fano.value.length == 0 || fano.value > ano){
-    }else{
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+       window.alert('[ERRO]')
+   
+       
+    }else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value) 
+        var idade = ano - Number(fano.value)
         var genero = ''
-
-        if (fsex[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
+        if(fsex[0].checked){
             genero = 'Homem'
+            if ( idade >=0 && idade <10 ){
+                //crinça
+                img.setAttribute('src', 'crinca-homem.png')
+            } else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-homem.png')
+            }else if( idade >= 21 && idade < 50 ){
+                //adulto
+
+            }else{
+                //idoso
+                img.setAttribute('src', 'idoso-homem.png')
+            }
+
         }else if(fsex[1].checked){
             genero = 'Mulher'
+            if ( idade >=0 && idade <10 ){
+                //criança
+                img.setAttribute('src', 'crinca-mulher.png')
+            } else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-mulher.png')
+            }else if( idade >= 21 && idade < 50 ){
+                //adulto
+            }else{
+                //idoso
+                img.setAttribute('src', 'idoso-mulher.png')
+            }
         }
-    
         res.innerHTML(`Detectamos ${genero} com ${idade} Anos`)
-        
+        res.appendChild(img)
+            
+    } 
+    
     }
-}
+        
+
