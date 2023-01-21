395757420fababdfc4b55b53e041f90bcddb2cf0
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-01-20 22:12:25 -0300

    .

---
+++ aula018/ambinte.js
@@ -0,0 +1,13 @@
+
+let a = [5, 6, 7]
+a.sort()
+a[3]= 8
+a.push(9)
+console.log(` o vetor tem ${a.length} posiçães`)
+let pos = a.indexOf(10)
+if(pos == -1){
+    console.log('O valor não foi encontrado')
+
+}else[
+    console.log(`O valor foi encontrado no ${pos} posição`)
+]
---
+++ aula018/vetornatela.js
@@ -0,0 +1,12 @@
+let valores= [1,2,3,4,5,6,7,8,9]
+
+/*console.log(valores)
+
+for(let pos = 0; pos < valores.length; pos++){
+    
+}
+*/
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+}
